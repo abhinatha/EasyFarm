3499765f81eabf860890293b5a58dfcbbe27b24f
abhinatha <169003829+abhinatha@users.noreply.github.com>
2026-07-22 07:45:24 -0400

    Customizations

--- EasyFarm/Infrastructure/AppBoot.cs
+++ EasyFarm/Infrastructure/AppBoot.cs
@@ -1,73 +1,45 @@
-﻿// ///////////////////////////////////////////////////////////////////
-// This file is a part of EasyFarm for Final Fantasy XI
-// Copyright (C) 2013 Mykezero
-//  
-// EasyFarm is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//  
-// EasyFarm is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// If not, see <http://www.gnu.org/licenses/>.
-// ///////////////////////////////////////////////////////////////////
-
-using System;
-using EasyFarm.Classes;
-using EasyFarm.Logging;
-using EasyFarm.Persistence;
-using EasyFarm.UserSettings;
-using EasyFarm.ViewModels;
+﻿using EasyFarm.Classes;
+using EasyFarm.Handlers;
 using EasyFarm.Views;
+using Ninject;
 
 namespace EasyFarm.Infrastructure
 {
     public class AppBoot
     {
-        private readonly CompositeRoot _container;
+        public IKernel Container { get; set; }
         private readonly App _app;
 
         public AppBoot(App app)
         {
             _app = app;
-            _container = new CompositeRoot();
+            Container = CreateContainer();
             _app.MainWindow = new MasterView();
         }
 
         public void Initialize()
         {
             SystemTray.ConfigureTray(_app.MainWindow);
-            LogViewModel.Write("Resources loaded");
-            LogViewModel.Write("Application starting");
-            Logger.Log(new LogEntry(LoggingEventType.Information, "EasyFarm Started ..."));
+        }
+
+        public object ViewModel => _app.MainWindow?.DataContext;
+
+        private IKernel CreateContainer()
+        {
+            StandardKernel container = new StandardKernel(
+                new ApplicationBindingsModule());
+            return container;
         }
 
         public void Navigate<TViewModel>()
         {
-            _app.MainWindow.DataContext = _container.Get(typeof(TViewModel));
+            NavigateViewRequestHandler handler = new NavigateViewRequestHandler(_app, Container);
+            handler.Handle(typeof(TViewModel)).GetAwaiter();
         }
 
         public void Show()
         {
             _app.MainWindow?.Show();
-        }
-
-        public void Exit()
-        {
-            IPersister persister = _container.Get<IPersister>();
-            string characterName = ViewModelBase.FFACE?.Player?.Name;
-            string fileName = $"{characterName}.eup";
-
-            if (!String.IsNullOrWhiteSpace(fileName))
-            {
-                persister.Serialize(fileName, Config.Instance);
-            }
-
-            LogViewModel.Write("Application exiting");
         }
     }
 }
--- EasyFarm/Infrastructure/TabViewModels.cs
+++ EasyFarm/Infrastructure/TabViewModels.cs
@@ -1,24 +1,8 @@
-﻿// ///////////////////////////////////////////////////////////////////
-// This file is a part of EasyFarm for Final Fantasy XI
-// Copyright (C) 2013 Mykezero
-//  
-// EasyFarm is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//  
-// EasyFarm is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// If not, see <http://www.gnu.org/licenses/>.
-// ///////////////////////////////////////////////////////////////////
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using EasyFarm.ViewModels;
+using Ninject;
 
 namespace EasyFarm.Infrastructure
 {
@@ -26,9 +10,13 @@ namespace EasyFarm.Infrastructure
     {
         public IList<IViewModel> ViewModels = new List<IViewModel>();
 
-        public TabViewModels(IList<IViewModel> viewModels)
+        public TabViewModels(IKernel container)
         {
-            ViewModels = viewModels;
+            foreach (KeyValuePair<Type, int> availableTab in AvailableTabs)
+            {
+                ViewModels.Add((IViewModel)container.Get(availableTab.Key));
+            }
+
             ViewModels = ViewModels
                 .Where(x => AvailableTabs.ContainsKey(x.GetType()))
                 .OrderBy(x => AvailableTabs[x.GetType()])
@@ -44,9 +32,11 @@ namespace EasyFarm.Infrastructure
             { typeof(TargetingViewModel), 2 },
             { typeof(RestingViewModel), 3 },
             { typeof(RoutesViewModel), 4 },
-            { typeof(FollowViewModel), 5 },
-            { typeof(LogViewModel), 6 },
-            { typeof(SettingsViewModel), 7 }
+            { typeof(CampViewModel), 5 },
+            { typeof(FollowViewModel), 6 },
+            { typeof(LogViewModel), 7 },
+            { typeof(WhitelistViewModel), 8 },
+            { typeof(SettingsViewModel), 9 }
         };
     }
 }
